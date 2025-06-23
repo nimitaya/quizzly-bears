98bf828e6c2347f54d27ce7ed37b12346a64e8ad
Natallia-Karatava <nkaratova@gmail.com>
2025-06-23 13:45:27 +0200

    add search friend button and input component

--- client/components/Inputs.tsx
+++ client/components/Inputs.tsx
@@ -6,16 +6,18 @@ import {
   useWindowDimensions,
 } from "react-native";
 import { Colors, FontSizes } from "../styles/theme";
-export default function Input(props: TextInputProps) {
+import IconSearchFriend from "@/assets/icons/IconSearchFriend";
+import { ButtonSearchFriend } from "./Buttons";
+
+// 1. Universal Search Input
+export function SearchInput(props: TextInputProps) {
   const { width } = useWindowDimensions();
-  const isSmallScreen = width < 348;
+  const inputWidth = Math.min(348, width - 32);
 
   return (
-    <View
-      style={[styles.container, isSmallScreen && { paddingHorizontal: 16 }]}
-    >
+    <View style={[styles.containerSearch, { width: inputWidth }]}>
       <TextInput
-        style={styles.input}
+        style={styles.inputSearch}
         placeholderTextColor={Colors.disable}
         {...props}
       />
@@ -23,13 +25,29 @@ export default function Input(props: TextInputProps) {
   );
 }
 
+// 2. Input with search button
+
+export function SearchFriendInput(props: TextInputProps) {
+  return (
+    <View style={styles.containerSearchFriend}>
+      <TextInput
+        style={styles.inputSearchFriend}
+        placeholderTextColor={Colors.disable}
+        {...props}
+      />
+      <View style={{ width: 8 }} />
+      <ButtonSearchFriend />
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
+  containerSearch: {
     width: "100%",
     maxWidth: 348,
     alignSelf: "center",
   },
-  input: {
+  inputSearch: {
     height: 56,
     borderRadius: 50,
     backgroundColor: Colors.bgGray,
@@ -38,6 +56,29 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: Colors.primaryLimo,
     width: "100%",
+    paddingHorizontal: 32,
+    fontSize: FontSizes.TextLargeFs,
+  },
+  containerSearchFriend: {
+    width: "100%",
+    alignSelf: "center",
+    position: "relative",
+    flexDirection: "row",
+    alignItems: "center",
+    maxWidth: 348,
+  },
+  inputSearchFriend: {
+    height: 56,
+    borderTopLeftRadius: 50,
+    borderBottomLeftRadius: 50,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+    backgroundColor: Colors.bgGray,
+    color: Colors.black,
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderColor: Colors.primaryLimo,
+    flex: 1,
     paddingHorizontal: 32,
     fontSize: FontSizes.TextLargeFs,
   },
