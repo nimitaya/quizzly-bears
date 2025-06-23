a7d13d722873cab5c934e32b67ea1da5833f3bc9
Natallia-Karatava <nkaratova@gmail.com>
2025-06-23 11:35:09 +0200

    update style

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,12 +1,3 @@
-import {
-  ButtonPrimary,
-  ButtonPrimaryDisabled,
-  ButtonSecondary,
-  ButtonSkip,
-  ButtonSmallPrimary,
-  ButtonSmallSecondary,
-} from "@/components/Buttons";
-import { QuizButton } from "@/components/QuizButtons";
 import { Text, View } from "react-native";
 
 // This can be the Loading Screen at the beginning of the app
