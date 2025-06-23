2d8f8d85f3b66f567c893cf52c3127f9ffa07058
Natallia-Karatava <nkaratova@gmail.com>
2025-06-23 11:29:29 +0200

    update style

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,3 +1,11 @@
+import {
+  ButtonPrimary,
+  ButtonPrimaryDisabled,
+  ButtonSecondary,
+  ButtonSkip,
+  ButtonSmallPrimary,
+  ButtonSmallSecondary,
+} from "@/components/Buttons";
 import { Text, View } from "react-native";
 
 // This can be the Loading Screen at the beginning of the app
@@ -12,6 +20,12 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <ButtonPrimary text="Play alone" />
+      <ButtonSecondary text="Play with friends" />
+      <ButtonPrimaryDisabled text="Play with (disabled)" />
+      <ButtonSkip text="Skip" />
+      <ButtonSmallPrimary text="Yes" />
+      <ButtonSmallSecondary text="No" />
     </View>
   );
 }
--- client/components/Buttons.tsx
+++ client/components/Buttons.tsx
@@ -4,36 +4,181 @@ import {
   StyleSheet,
   Text,
   View,
+  useWindowDimensions,
 } from "react-native";
-import { Colors, Radius } from "../styles/theme";
+import { Colors, Radius, FontSizes } from "../styles/theme";
 
 export function ButtonPrimary({
   text,
   ...props
 }: PressableProps & { text: string }) {
+  const { width } = useWindowDimensions();
+  const buttonWidth = Math.min(348, width - 32);
+
   return (
     <Pressable {...props}>
-      <View style={styles.buttonPrimary}>
-        <Text style={styles.text}>{text}</Text>
+      <View style={[styles.buttonPrimary, { width: buttonWidth }]}>
+        <Text style={styles.textPrimaryButton}>{text}</Text>
       </View>
     </Pressable>
   );
 }
+export function ButtonPrimaryDisabled({
+  text,
+  ...props
+}: PressableProps & { text: string }) {
+  const { width } = useWindowDimensions();
+  const buttonWidth = Math.min(348, width - 32);
 
+  return (
+    <Pressable {...props}>
+      <View style={[styles.buttonPrimaryDisabled, { width: buttonWidth }]}>
+        <Text style={styles.textPrimaryButtonDisabled}>{text}</Text>
+      </View>
+    </Pressable>
+  );
+}
+export function ButtonSecondary({
+  text,
+  ...props
+}: PressableProps & { text: string }) {
+  const { width } = useWindowDimensions();
+  const buttonWidth = Math.min(348, width - 32);
+
+  return (
+    <Pressable {...props}>
+      <View style={[styles.buttonSecondary, { width: buttonWidth }]}>
+        <Text style={styles.textSecondaryButton}>{text}</Text>
+      </View>
+    </Pressable>
+  );
+}
+export function ButtonSkip({
+  text,
+  ...props
+}: PressableProps & { text: string }) {
+  const { width } = useWindowDimensions();
+  const buttonWidth = Math.min(348, width - 32);
+
+  return (
+    <Pressable {...props}>
+      <View style={[styles.buttonSkip, { width: buttonWidth }]}>
+        <Text style={styles.textSkipButton}>{text}</Text>
+      </View>
+    </Pressable>
+  );
+}
+export function ButtonSmallPrimary({
+  text,
+  ...props
+}: PressableProps & { text: string }) {
+  const { width } = useWindowDimensions();
+  const buttonWidth = Math.min(120, width - 16);
+
+  return (
+    <Pressable {...props}>
+      <View style={[styles.buttonSmallPrimary, { width: buttonWidth }]}>
+        <Text style={styles.textSmallPrimaryButton}>{text}</Text>
+      </View>
+    </Pressable>
+  );
+}
+export function ButtonSmallSecondary({
+  text,
+  ...props
+}: PressableProps & { text: string }) {
+  const { width } = useWindowDimensions();
+  const buttonWidth = Math.min(120, width - 16);
+
+  return (
+    <Pressable {...props}>
+      <View style={[styles.buttonSmallSecondary, { width: buttonWidth }]}>
+        <Text style={styles.textSmallSecondaryButton}>{text}</Text>
+      </View>
+    </Pressable>
+  );
+}
 const styles = StyleSheet.create({
   buttonPrimary: {
     backgroundColor: Colors.primaryLimo,
     justifyContent: "center",
     alignItems: "center",
     padding: 16,
-    width: "100%",
-    maxWidth: 348,
     height: 56,
     alignSelf: "center",
     borderRadius: Radius.r50,
   },
-  text: {
-    fontSize: 16,
+  textPrimaryButton: {
+    fontSize: FontSizes.TextLargeFs,
     color: Colors.black,
+  },
+
+  buttonPrimaryDisabled: {
+    backgroundColor: Colors.disablelLimo,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    height: 56,
+    alignSelf: "center",
+    borderRadius: Radius.r50,
+  },
+  textPrimaryButtonDisabled: {
+    fontSize: FontSizes.TextLargeFs,
+    color: Colors.disable,
+  },
+
+  buttonSecondary: {
+    backgroundColor: Colors.white,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    height: 56,
+    alignSelf: "center",
+    borderRadius: Radius.r50,
+  },
+  textSecondaryButton: {
+    fontSize: FontSizes.TextLargeFs,
+    color: Colors.darkGreen,
+  },
+
+  buttonSkip: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    height: 56,
+    alignSelf: "center",
+    borderRadius: Radius.r50,
+  },
+  textSkipButton: {
+    fontSize: FontSizes.TextLargeFs,
+    color: Colors.darkGreen,
+  },
+  buttonSmallPrimary: {
+    backgroundColor: Colors.primaryLimo,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 8,
+    height: 48,
+    width: 120,
+    alignSelf: "center",
+    borderRadius: Radius.r50,
+  },
+  textSmallPrimaryButton: {
+    fontSize: FontSizes.TextMediumFs,
+    color: Colors.black,
+  },
+  buttonSmallSecondary: {
+    backgroundColor: Colors.white,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 8,
+    height: 48,
+    width: 120,
+    alignSelf: "center",
+    borderRadius: Radius.r50,
+  },
+  textSmallSecondaryButton: {
+    fontSize: FontSizes.TextMediumFs,
+    color: Colors.darkGreen,
   },
 });
