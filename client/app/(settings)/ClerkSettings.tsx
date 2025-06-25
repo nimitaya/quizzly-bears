d0bf273275c51fcb1760f3faf1c278e7e20ccac0
marrozhkova <mar.rozhkova@gmail.com>
2025-06-25 11:19:27 +0200

    clerk

--- client/app/(settings)/ClerkSettings.tsx
+++ client/app/(settings)/ClerkSettings.tsx
@@ -1,9 +1,9 @@
 import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
-import { SignOutButton } from "../../components/SignOutButton";
+import { SignOutButton } from "@/components/SignOutButton";
 
-export default function Page() {
+export default function ClerlSettings() {
   const { user } = useUser();
 
   return (
--- client/app/(tabs)/profile/index.tsx
+++ client/app/(tabs)/profile/index.tsx
@@ -1,12 +1,14 @@
 import IconBearTab from "@/assets/icons/IconBearTab";
 import IconBearTabAktiv from "@/assets/icons/IconBearTabAktiv";
 import { Text, View } from "react-native";
+import ClerlSettings from "@/app/(settings)/ClerkSettings";
 const ProfileScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>ProfileScreen</Text>
       <IconBearTab />
       <IconBearTabAktiv />
+      <ClerlSettings />
     </View>
   );
 };
