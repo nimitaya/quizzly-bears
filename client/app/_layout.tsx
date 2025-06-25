7f227b4175e116f85246c57fbe3f4c591e2d2427
marrozhkova <mar.rozhkova@gmail.com>
2025-06-25 08:23:28 +0200

    clerk

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,12 +1,16 @@
 import { Stack } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
 // For all routes and the root of the grouped-routes
 export default function RootLayout() {
   return (
-    <Stack>
-      {/* First Example - All pages we want to use go in here */}
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack>
+        {/* First Example - All pages we want to use go in here */}
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </ClerkProvider>
   );
 }
