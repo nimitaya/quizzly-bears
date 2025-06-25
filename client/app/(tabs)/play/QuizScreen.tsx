e7b7208897ef8db886caf7e03837975320fa3878
Natallia-Karatava <nkaratova@gmail.com>
2025-06-25 14:17:23 +0200

    Refactor QuizScreen layout and styles for improved readability and consistency

--- client/app/(tabs)/play/QuizScreen.tsx
+++ client/app/(tabs)/play/QuizScreen.tsx
@@ -2,6 +2,7 @@ import { View, Text, TouchableHighlight, StyleSheet } from "react-native";
 import { QuizButton } from "@/components/QuizButtons";
 import { ButtonPrimary, ButtonPrimaryDisabled } from "@/components/Buttons";
 import { useQuizLogic } from "@/utilities/quiz-logic/useQuizLogic";
+import { Colors, FontSizes, Gaps } from "@/styles/theme";
 
 const QuizLogic = () => {
   const {
@@ -36,47 +37,54 @@ const QuizLogic = () => {
         </View>
       ) : (
         <View style={styles.container}>
-          <View>
-            <Text>{currQuestionIndex + 1}/10</Text>
-            <Text>{currentQuestionData?.question}</Text>
+          <View style={styles.questionScreenContainer}>
+            <Text style={styles.questionNumber}>
+              {currQuestionIndex + 1} from 10
+            </Text>
+            <Text style={styles.questionText}>
+              {currentQuestionData?.question}
+            </Text>
           </View>
           {/* Show only if readTimer true */}
           {readTimer && (
             <View>
               <View>
                 <Text>Timer Anzeige TODO</Text>
+                <Text>Taste „Close“ oben links, die das Quiz beendet.</Text>
                 <Text>Time left: {remainingTime}s</Text>
               </View>
-              <View style={styles.answerContainer}>
-                {/* show one quiz button for each option */}
-                {currentQuestionData &&
-                  currentQuestionData.options.map((option, index) => (
-                    <QuizButton
-                      key={index}
-                      text={option}
-                      selected={handleSelection(option)}
-                      checked={
-                        (answerState.isLocked &&
-                          currentQuestionData.answer === option) ||
-                        (answerState.isLocked &&
-                          answerState.isSubmitted &&
-                          answerState.chosenAnswer === option)
-                      }
-                      isCorrect={currentQuestionData.answer === option}
-                      onPress={() => handleAnswerSelect(option)}
-                    />
-                  ))}
-              </View>
-              <View>
-                {answerState.isLocked && playStyle === "solo" ? (
-                  <ButtonPrimary text="Next" onPress={handleNextQuestion} />
-                ) : answerState.isLocked && playStyle === "group" ? (
-                  <ButtonPrimaryDisabled text="Waiting for other bears..." />
-                ) : answerState.isSelected ? (
-                  <ButtonPrimary text="Answer" onPress={handleAnswerSubmit} />
-                ) : (
-                  <ButtonPrimaryDisabled text="Answer" />
-                )}
+              <View style={styles.questionAnswerContainer}>
+                <View style={styles.answerContainer}>
+                  {/* show one quiz button for each option */}
+                  {currentQuestionData &&
+                    currentQuestionData.options.map((option, index) => (
+                      <QuizButton
+                        key={index}
+                        text={option}
+                        selected={handleSelection(option)}
+                        checked={
+                          (answerState.isLocked &&
+                            currentQuestionData.answer === option) ||
+                          (answerState.isLocked &&
+                            answerState.isSubmitted &&
+                            answerState.chosenAnswer === option)
+                        }
+                        isCorrect={currentQuestionData.answer === option}
+                        onPress={() => handleAnswerSelect(option)}
+                      />
+                    ))}
+                </View>
+                <View>
+                  {answerState.isLocked && playStyle === "solo" ? (
+                    <ButtonPrimary text="Next" onPress={handleNextQuestion} />
+                  ) : answerState.isLocked && playStyle === "group" ? (
+                    <ButtonPrimaryDisabled text="Waiting for other bears..." />
+                  ) : answerState.isSelected ? (
+                    <ButtonPrimary text="Answer" onPress={handleAnswerSubmit} />
+                  ) : (
+                    <ButtonPrimaryDisabled text="Answer" />
+                  )}
+                </View>
               </View>
             </View>
           )}
@@ -90,11 +98,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    paddingHorizontal: 10,
+    paddingHorizontal: Gaps.g32,
+    color: Colors.black,
   },
   answerContainer: {
-    gap: 10,
+    gap: Gaps.g8,
   },
+  questionNumber: {
+    textAlign: "center",
+    color: Colors.black,
+    fontSize: FontSizes.H3Fs,
+    paddingBottom: Gaps.g16,
+  },
+  questionText: {
+    color: Colors.black,
+    width: "100%",
+    height: 135,
+    fontSize: FontSizes.H3Fs,
+    paddingBottom: Gaps.g32,
+    textAlign: "center",
+  },
+  questionAnswerContainer: {
+    gap: Gaps.g16,
+  },
+  questionScreenContainer: {},
 });
 
 export default QuizLogic;
