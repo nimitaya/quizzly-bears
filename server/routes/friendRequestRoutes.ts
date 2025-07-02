ff03b6f696c58b8c4215dc0a88c039a4ff3fbf59
nimitaya <sonja.schweren@web.de>
2025-07-02 15:43:14 +0200

    sort friend request routes

--- server/routes/friendRequestRoutes.ts
+++ server/routes/friendRequestRoutes.ts
@@ -13,12 +13,12 @@ import {
 const friendRequestRouter = express.Router();
 
 friendRequestRouter.get("/search", searchUser);
-friendRequestRouter.post("/send", sendRequest);
 friendRequestRouter.get("/received", getReceivedRequests);
 friendRequestRouter.get("/sent", getSentRequests);
+friendRequestRouter.get("/friends", getFriendList)
+friendRequestRouter.post("/send", sendRequest);
 friendRequestRouter.post("/accept", acceptRequest);
 friendRequestRouter.post("/decline", declineRequest);
-friendRequestRouter.get("/friends", getFriendList)
 friendRequestRouter.delete("/remove", removeFriend)
 
 export default friendRequestRouter;
